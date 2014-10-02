9d3a4e740bc6541bcdf280dcf59e986fe104a242
Christian Fobel <christian@fobel.net>
2014-10-02 13:46:31 -0400

    Use I2CHandler from ArduinoRPC library

--- i2c_rpc/Arduino/i2c_rpc/i2c_rpc.ino
+++ i2c_rpc/Arduino/i2c_rpc/i2c_rpc.ino
@@ -1,6 +1,7 @@
 #include "EEPROM.h"
 #include "Wire.h"
 #include "Memory.h"
+#include "I2CHandler.h"
 #include "nanopb.h"
 #include "NadaMQ.h"
 #include "ArduinoRPC.h"
@@ -18,15 +19,12 @@
 uint8_t packet_buffer[PACKET_SIZE];
 #endif  // #ifndef DISABLE_SERIAL
 
+uint8_t i2c_packet_buffer[PACKET_SIZE];
+
 /*  - Allocate buffer for command-processor to extract/write array data. */
 uint8_t command_array_buffer[COMMAND_ARRAY_BUFFER_SIZE];
 UInt8Array command_array = {COMMAND_ARRAY_BUFFER_SIZE,
                             &command_array_buffer[0]};
-
-uint8_t i2c_packet_buffer[PACKET_SIZE];
-uint8_t processing_i2c_request = false;
-uint8_t i2c_response_size_sent = false;
-FixedPacket i2c_packet;
 
 Node node;
 CommandProcessor<Node> command_processor(node, command_array);
@@ -53,8 +51,8 @@ void setup() {
   Wire.begin();
 #else
   /* Join I2C bus as slave. */
-  Wire.onReceive(i2c_receive_event);
-  Wire.onRequest(i2c_request_event);
+  Wire.onReceive(&I2CHandlerClass::i2c_receive_event);
+  Wire.onRequest(&I2CHandlerClass::i2c_request_event);
 #endif  // #ifdef __AVR_ATmega328__
   // Set i2c clock-rate to 400kHz.
   TWBR = 12;
@@ -63,7 +61,7 @@ void setup() {
   packet.reset_buffer(PACKET_SIZE, &packet_buffer[0]);
   parser.reset(&packet);
 #endif  // #ifndef DISABLE_SERIAL
-  i2c_packet.reset_buffer(PACKET_SIZE, &i2c_packet_buffer[0]);
+  I2CHandler.begin(i2c_packet_buffer, PACKET_SIZE);
 }
 
 
@@ -74,39 +72,5 @@ void loop() {
    * process the request. */
   reactor.parse_available();
 #endif  // #ifndef DISABLE_SERIAL
-  if (processing_i2c_request) {
-    process_packet_with_processor(i2c_packet, command_processor);
-    processing_i2c_request = false;
-  }
-}
-
-
-void i2c_receive_event(int byte_count) {
-  processing_i2c_request = true;
-  /* Record all bytes received on the i2c bus to a buffer.  The contents of
-   * this buffer will be forwarded to the local serial-stream. */
-  int i;
-  for (i = 0; i < byte_count; i++) {
-      i2c_packet_buffer[i] = Wire.read();
-  }
-  i2c_packet.payload_length_ = i;
-  i2c_packet.type(Packet::packet_type::DATA);
-}
-
-
-void i2c_request_event() {
-  uint8_t byte_count = (uint8_t)i2c_packet.payload_length_;
-  /* There is a response from a previously received packet, so send it to the
-   * master of the i2c bus. */
-  if (!i2c_response_size_sent) {
-    if (processing_i2c_request) {
-      Wire.write(0xFF);
-    } else {
-      Wire.write(byte_count);
-      i2c_response_size_sent = true;
-    }
-  } else {
-    Wire.write(i2c_packet.payload_buffer_, byte_count);
-    i2c_response_size_sent = false;
-  }
+  I2CHandler.parse_available(command_processor);
 }
